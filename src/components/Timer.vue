baf14889c18a5d1d59282f94aaf417c80ccd3595
kachid <36529183+kachid@users.noreply.github.com>
2019-01-11 14:55:01 +0500

    add components

---
+++ src/components/Timer.vue
@@ -0,0 +1,24 @@
+<template>
+    <h2 id="timer">{{msg}}</h2>
+</template>
+
+<script>
+    export default {
+        name: 'Timer',
+        props: {
+          msg: String
+        }
+    }
+</script>
+
+<style scoped>
+    #timer {
+        margin: 30px auto;
+        text-align: center;
+
+        font-family: Arial, sans-serif;
+        font-size: 32px;
+        line-height: 36px;
+        color: #434344;
+    }
+</style>
